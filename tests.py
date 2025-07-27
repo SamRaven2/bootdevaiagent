903ba413d2f6e309ef2fc2ed4eb9484162ff322f
Bats <samraven2@gmail.com>
2025-07-26 20:52:41 -0400

    Updatiing tests for clarity of output

--- tests.py
+++ tests.py
@@ -1,13 +1,17 @@
 from functions.get_files_info import get_files_info
 
 def run_tests():
-    print(get_files_info("calculator", "."))
+    print("Running tests on \".\" as directory.")
+    print(get_files_info("calculator", ".") + "\n")
 
-    print(get_files_info("calculator", "pkg"))
+    print("Running tests on \"pkg\" as directory.")
+    print(get_files_info("calculator", "pkg") + "\n") 
 
-    print(get_files_info("calculator", "/bin"))
+    print("Running tests on \"/bin\" as directory.")
+    print(get_files_info("calculator", "/bin") + "\n")
 
-    print(get_files_info("calculator", "../"))
+    print("Running tests on \"../\" as directory.")
+    print(get_files_info("calculator", "../") + "\n")
 
     print(f"All tests complete")
 
